a2bf2ed6b8d9a5761ed2abb4d7b7e7ef2207d8a7
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-11-01 14:25:46 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
-// Imports
 import "fmt"
 
 func main() {
-	fmt.Println("Olá, pessoal!")
-	fmt.Println("Tudo bem?")
-}	
+	var nome string = "Diêgo"
 
+	fmt.Println("Olá, mundo", nome)
+}
